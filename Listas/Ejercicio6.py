1689add1af8cc8c01c61b5f4ac8d5091703d8b4e
pareja31 <116192640+pareja31@users.noreply.github.com>
2023-03-31 18:25:30 -0500

    Delete Listas directory

--- Listas/Ejercicio6.py
+++
@@ -1,9 +0,0 @@
-#este ejercicio no esta realizado por mi
-
-import random
-rang = int(input("Cuantos numero desea que se muestren "))
-fib=[0,1]
-for i in range(rang-2):
-    fib.append(fib[-1]+fib[-2])
-print(fib)    
-
