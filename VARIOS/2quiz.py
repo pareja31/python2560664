ecf6daecb077474658be123ca1bdef989b649081
pareja31 <116192640+pareja31@users.noreply.github.com>
2023-03-31 18:25:39 -0500

    Delete VARIOS directory

--- VARIOS/2quiz.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Ingrese numero"))
-c=n-1
-while (n<c):
-    r=n*c
-print("r")    
-
--- VARIOS/8.py
+++
@@ -1,8 +0,0 @@
-banderazo = 200
-m = int(input('Ingrese los minutos que druó de la llamada'))
-if m <= 3:
-    print('La llamada de fue de',m, 'minutos, por lo cual el pago será de',banderazo)
-else:
-    me = m-3
-    total = (me*50) + banderazo
-    print('La llamada de fue de',m, 'minutos, por lo cual el pago será de',total)
